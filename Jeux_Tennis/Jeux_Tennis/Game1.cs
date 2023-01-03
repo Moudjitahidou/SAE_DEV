86e994bf01c9b703d492a4da33e74c30cfbe0a02
Moudjitahidou Boinaidi <Moudjitahidou.Boinaidi@etu.univ-smb.fr>
2023-01-03 16:18:51 +0100

    premier

---
+++ Jeux_Tennis/Jeux_Tennis/Game1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+
+namespace Jeux_Tennis
+{
+    public class Game1 : Game
+    {
+
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _tiledMap = Content.Load<TiledMap>("mapGenerale");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            _tiledMapRenderer.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _tiledMapRenderer.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Jeux_Tennis/Jeux_Tennis/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Jeux_Tennis.Game1();
+game.Run();
